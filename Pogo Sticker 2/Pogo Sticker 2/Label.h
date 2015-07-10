4e2e9b0364e7319461827580c536fb326b96d771
Jonathan-92 <jonte790@gmail.com>
2015-07-10 15:54:56 +0200

    added comments

--- Pogo Sticker 2/Pogo Sticker 2/Label.h
+++ Pogo Sticker 2/Pogo Sticker 2/Label.h
@@ -1,27 +1,40 @@
 #ifndef LABEL_H
 #define LABEL_H
+
 #include "Sprite.h"
 #include <string>
 
 namespace gameEngine {
 
-class Label :
-	public Sprite
-{
-public:
-	typedef std::string (*FuncPtr)();
-	static Label* getInstance(int x, int y, int w, int h, std::string text, FuncPtr fp);
-	static Label* getInstance(int x, int y, int w, int h, std::string text);
-	virtual void tick();
-	void setText(std::string text);
-	~Label();
-protected:
-	Label(int x, int y, int w, int h, std::string text, FuncPtr fp);
-	Label(int x, int y, int w, int h, std::string text);
-	static const SDL_Color black;
-private:
-	FuncPtr funcPtr;
-};
+	/* A text label which can be updated. Also a function can be supplied for
+	additional behaviour. */
+	class Label : public Sprite
+	{
+	public:
+		/* A name for a function pointer is defined for easier reference. */
+		typedef std::string(*FuncPtr)();
+
+		/* Protected constructors (allow subclasses) and public getInstance
+		methods to only allow dynamic instantiation of this class. */
+		static Label* getInstance(int x, int y, int w, int h, std::string text,
+			FuncPtr fp);
+		static Label* getInstance(int x, int y, int w, int h, std::string text);
+
+		void tick(); // Inherited from Sprite
+		std::string getText();
+		void setText(std::string text);
+		~Label();
+	protected:
+		Label(int x, int y, int w, int h, std::string text, FuncPtr fp);
+		Label(int x, int y, int w, int h, std::string text);
+	private:
+		Label(const Label&);		   			/* Value semantics is disabled. */
+		const Label& operator=(const Label&);	/* Value semantics is disabled. */
+
+		static const SDL_Color black;
+		std::string text;
+		FuncPtr funcPtr;
+	};
 
 }
 
--- Pogo Sticker 2/Pogo Sticker 2/Sprite.h
+++ Pogo Sticker 2/Pogo Sticker 2/Sprite.h
@@ -5,25 +5,58 @@
 
 namespace gameEngine {
 
+	/* An abstract class that represents a visual (or non-visual) figure in the game.
+	It can react on events such as mouse presses. What it does all depends on the
+	implementation of the subclasses derived from this class. */
 	class Sprite {
 	public:
-		virtual ~Sprite();
+		/* This is a rectangle specifying the width, height, x, and y coordinate
+		of the Sprite. */
 		Rect rect;
+
+		/* Draws the Sprite on the window. This function can be overridden
+		if special behaviour is desired. */
 		virtual void draw();
+
+		/* Performs something each lap in the game loop. What it does depends
+		on the implementation of the subclasses to this class. */
 		virtual void tick(){}
+
+		/* Call this function when a mouse button is clicked and pass the x and y
+		coordinates of the event. */
 		virtual void mouseDown(int x, int y){}
+
+		/* Call this function when a mouse button is pressed and pass the x and y
+		coordinates of the event. */
+		virtual void mousePressed(int x, int y){}
+
 		virtual void mouseMotion(int x, int y){}
+
+		/* Call this function when a key on the keyboard is pressed and pass the
+		event */
 		virtual void keyDown(SDL_Event& e){}
-		virtual void keyUp(SDL_Event& e){}
+
 		virtual void collisonCheck(gameEngine::Sprite *sprites[]){}
 		virtual int getPosX();
 		virtual int getPosY();
 	protected:
+		virtual ~Sprite();
+
+		/* This is what will be copied to the renderer and displayed in the
+		window. */
 		SDL_Texture* texture;
+
+		// Creates a Sprite from an image path, and uses the flag transp to 
+		// determine if the background should be transparent of not
 		Sprite(int x, int y, int w, int h, std::string imgPath, bool transp);
+
+		// Creates a Sprite from an SDL_Surface*
 		Sprite(int x, int y, int w, int h, SDL_Surface* surface);
+
+		// Allows for creating a Sprite that has no visibility properties
 		Sprite();
 	private:
+		/* Value semantics is disabled. */
 		Sprite(const Sprite&);
 		const Sprite& operator=(const Sprite&);
 	};
